6bdc3e15cb8f20fdd5659c942c26e93355e1430f
chan lap ping <chanlapping64@gmail.com>
2023-04-03 20:46:17 +0800

    Add delete method.

--- script.js
+++ script.js
@@ -24,7 +24,37 @@ class Tree {
             node.left = this.#insertValue(node.left, value);
         } else if (value > node.data) {
             node.right = this.#insertValue(node.right, value);
-        } 
+        }
+        return node;
+    }
+
+    delete(value) {
+        this.#deleteNode(this.root, value);
+    }
+
+    #deleteNode(node, value) {
+        if (node == null) {
+            return node;
+        }
+        if (value < node.data) {
+            node.left = this.#deleteNode(node.left, value);
+        } else if (value > node.data) {
+            node.right = this.#deleteNode(node.right, value);
+        } else {
+            if (node.left == null) {
+                return node.right;
+            }
+            if (node.right == null) {
+                return node.left;
+            }
+    
+            let succ = node.right;
+            while (succ.left) {
+                succ = succ.left;
+            }
+            node.data = succ.data;
+            node.right = this.#deleteNode(node.right, node.data);
+        }
         return node;
     }
 }
@@ -33,7 +63,7 @@ function buildTree(arr) {
     let set = new Set(arr);
     let newArr = Array.from(set);
     newArr.sort((a, b) => a - b);
-    
+
     const root = buildBST(newArr, 0, newArr.length - 1);
     return root;
 }
@@ -42,7 +72,7 @@ function buildBST(arr, start, end) {
     if (start > end) {
         return null;
     }
-    let mid = Math.floor((start + end)/2);
+    let mid = Math.floor((start + end) / 2);
     let node = new Node(arr[mid]);
     node.left = buildBST(arr, start, mid - 1);
     node.right = buildBST(arr, mid + 1, end);
@@ -51,19 +81,21 @@ function buildBST(arr, start, end) {
 
 const prettyPrint = (node, prefix = '', isLeft = true) => {
     if (node === null) {
-       return;
+        return;
     }
     if (node.right !== null) {
-      prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+        prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
     }
     console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
     if (node.left !== null) {
-      prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+        prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
     }
-  }
+}
 
-  let a = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
-  let tree = new Tree(a);
-  prettyPrint(tree.root);
-  tree.insert(68);
-  prettyPrint(tree.root);
+
+
+let a = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+let tree = new Tree(a);
+prettyPrint(tree.root);
+tree.delete(67);
+prettyPrint(tree.root);
